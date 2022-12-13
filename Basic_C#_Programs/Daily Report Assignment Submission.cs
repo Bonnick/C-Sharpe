7294403c70b3829dac25272a92acdf1e2cf47a5a
Bonnick <bonnickromario@gmail.com>
2022-12-13 14:54:08 -0500

    Update Daily Report Assignment Submission.cs

--- Basic_C#_Programs/Daily Report Assignment Submission.cs
+++ Basic_C#_Programs/Daily Report Assignment Submission.cs
@@ -23,18 +23,23 @@ namespace Daily_Report
             //This console command ask the page number of the course.
             Console.WriteLine("What page number?");
             string pagenumber = Console.ReadLine();
-            Console.WriteLine("Great" + pagenumber + "!");
+            Console.WriteLine("Please provide the Page number" + "!");
             Console.Read();
             //This command help us to understand that the student needs help with the course
             bool Doyouneedhelpwithanything = true;
             Console.WriteLine(Doyouneedhelpwithanything);
             // This command is basically asking a question
             Console.WriteLine("Were there any positive experience you'd like to share? Please give specifics");
+            string No = Console.ReadLine();
+            Console.WriteLine("Thank you");
+            Console.Read();
 
             Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific");
+            string Yes = Console.ReadLine();
+            Console.WriteLine("Type below");
+            Console.Read();
             Console.ReadLine();
-            //This command is telling us how much hours per day the student study
-            byte hoursStudy = 10;
+        
             //This command is a statement thanking you for your responses. 
             Console.WriteLine("Thanks you for your answers. An instructor will respond shortly. Have a great day");
             Console.ReadLine();
