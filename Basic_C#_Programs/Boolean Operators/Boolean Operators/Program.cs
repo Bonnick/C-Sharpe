1332ed9bc9d20e2cc9cfaa99075461bac4c9335b
Bonnick <bonnickromario@gmail.com>
2022-12-18 20:18:32 -0500

    Boolean Operator

---
+++ Basic_C#_Programs/Boolean Operators/Boolean Operators/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean_Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            //Console.WriteLine(true && true && true && false);
+            //Console.WriteLine(true && true && true || false);
+            //Console.ReadLine();
+            //The AND Operator
+            //Console.WriteLine(true && false);
+            //Console.WriteLine(true && true);
+            //Console.WriteLine(false && false);
+            //Console.ReadLine();
+
+            //Console.WriteLine(true || true);
+            //Console.WriteLine(true || false);
+            //Console.WriteLine(false || false);
+            //Console.ReadLine();
+
+
+            //Console.WriteLine(true == true);
+            //Console.WriteLine(true == false);
+            //Console.WriteLine(false == false);
+            //Console.ReadLine();
+
+            //Console.WriteLine(true != true);
+            //Console.WriteLine(true != false);
+            //Console.WriteLine(false != false);
+            //Console.ReadLine();
+          
+            //Console.WriteLine(true ^ true);
+            //Console.WriteLine(true ^ false);
+            //Console.WriteLine(false ^ false);
+            //Console.ReadLine();
+
+
+        }
+    }
+}
