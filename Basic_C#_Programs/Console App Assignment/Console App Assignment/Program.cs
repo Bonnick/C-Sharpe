884340ac6185f031cb8de88fb40af73efb042b5b
Bonnick <bonnickromario@gmail.com>
2022-12-21 19:39:38 -0500

    Console App Assignment

---
+++ Basic_C#_Programs/Console App Assignment/Console App Assignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_App_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is a do while statement
+
+            int count = 0;
+            do
+            {
+                Console.WriteLine("Inside the Loop");
+                count++;
+            }
+            while (count < 5);
+            int counting = 0;
+
+            //This is a while statement loop
+            while(counting <3)
+                {
+                Console.WriteLine(" counting numbers");
+                counting++;
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
