fc18f970274aa6ca1be4361f173cc0c1ea40c290
Bonnick <bonnickromario@gmail.com>
2023-01-02 17:37:16 -0500

    Part-Six Assignment

---
+++ Basic_C#_Programs/Six-Part Assignment/Six-Part Assignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Six_Part_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Assignment Part 1
+            string lineOne = "Beware; ";
+            string lineTwo = "for I am fearless, ";
+            string lineThree = "and therefore powerful. ";
+
+            //Concatenating the three strings:
+            Console.Write(lineOne + lineTwo + lineThree);
+
+            //Assignment Part 2
+            //A string to be converted to uppercase:
+            string x = "who controls the past controls the future. ";
+
+            //Converting string to uppercase:
+            Console.WriteLine(x.ToUpper());
+            Console.ReadLine();
+        }
+    }
+}
