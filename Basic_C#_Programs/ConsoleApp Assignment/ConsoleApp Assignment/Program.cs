b2e1a71da1edd0916b98a30862926ec50f2ee910
Bonnick <bonnickromario@gmail.com>
2023-01-04 21:02:11 -0500

    Console App Assignment Part 1-6

--- Basic_C#_Programs/ConsoleApp Assignment/ConsoleApp Assignment/Program.cs
+++ Basic_C#_Programs/ConsoleApp Assignment/ConsoleApp Assignment/Program.cs
@@ -10,70 +10,72 @@ namespace ConsoleApp_Assignment
         {
             // Part one
 
-            //String[] s = { "Nick", "Romario", "Marlesa", "Tom", "Dandan" };
-            //Console.WriteLine("Provide a word; ");
-            //string word = Console.ReadLine();
-            //for (int i = 0; i < s.Length; i++)
-            //{
-            //s[i] += word ;
-            //}
+            // A Array of String
+            String[] s = { "Nick", "Romario", "Marlesa", "Tom", "Dandan" };
+            //Ask the user to input some text
+            Console.WriteLine("Provide a word; ");
+            //Ask the loop to iterates through each string in the array and adds the user's text input to the end of each string.
+            string word = Console.ReadLine();
+            for (int i = 0; i < s.Length; i++)
+            {
+            s[i] += word ;
+            }
+            //Create a second loop that prints off each string in the array one at a time.
+            for (int i = 0; i < s.Length; i++)
+            {
+            Console.WriteLine(s[i]);
+            }
 
-            //for (int i = 0; i < s.Length; i++)
-            //{
-            //Console.WriteLine(s[i]);
-            //}
+            Console.ReadLine();  
 
-            //Console.ReadLine();  
-
-            //}
-            //}
-            //} 
+         }
+     }
+} 
 
             //Part two
 
             //The Infinite Loop
 
-            // Foreloop
+             Foreloop
 
-            //            for (; ; )
-            //            {
-            //                // The Forever young will run as the infinite loop in the body 
-            //                Console.WriteLine("Forever young!\n");
-            //            }
-            //        }
-            //    }
-            //}
+            for (; ; )
+            {
+            // An infinite Loop
+             Console.WriteLine("Forever young!\n");
+            }
+          }
+       }
+   }
+            //Fix the Infinite Loop so that it will execute properly
 
-
-            //Fix the Infinite Loop
-
-            //for (int i = 1; i < 5; i++)
-            //{
+            for (int i = 1; i < 5; i++)
+            {
             //The Forever young will run as the infinite loop in the body 
-            //Console.WriteLine("Forever young!\n");
-            //}
-            //Console.ReadLine();
-            // }
-            //}
-            //}
+            Console.WriteLine("Forever young!\n");
+            }
+            Console.ReadLine();
+           }
+         }
+      }
 
             // Part 3
-            //iterate the loop.
-            //List<int> grades = new List<int>() { 5, 10, 26, 34, 26, 26, 2, 23, 26, 22 };
+            //A loop where the comparison that's used to determine whether to continue iterating the loop is a "<" operator.
+            List<int> grades = new List<int>() { 5, 10, 26, 34, 26, 26, 2, 23, 26, 22 };
 
-            //for (int i = 0; i < grades.Count; i++)
-            //{
-            // Console.WriteLine(grades[i]);
-            //}
-            // for (int i = 0; i <= grades.Count -1; i++)
-            //{
-            //Console.WriteLine(grades[i]);
-            //}
-            //Console.ReadLine();
+            for (int i = 0; i < grades.Count; i++)
+            {
+            Console.WriteLine(grades[i]);
+            }
+            // Add another loop where the comparison that's used to determine whether to continue iterating the loop is a "<=" operator.
+            for (int i = 0; i <= grades.Count -1; i++)
+            {
+            Console.WriteLine(grades[i]);
+            }
+            Console.ReadLine();
 
-            //}
-            //}
-            //}
+           }
+         }
+       }
 
 
 
@@ -100,7 +102,7 @@ namespace ConsoleApp_Assignment
                     Console.WriteLine("The Letter you choose is at index " + i);
                 }
  
-
+                // Add code that stops the loop from executing once a match has been found.
             }
             if (Letters.Contains(word.ToUpper()) == false)
             {
@@ -113,74 +115,69 @@ namespace ConsoleApp_Assignment
 
 
 
-//Part 5
-//            List<string> Letters = new List<string>() { "A", "B", "C", "D", "C" };
-//            Console.WriteLine(" Please type one of these letters ; A,B,C,D,");
-//            string word = Console.ReadLine();
+                                    //Part 5
+                                   
+            // A list of strings that has at least two identical strings in the list. Ask the user to select text to search for in the list. 
+
+            List<string> Letters = new List<string>() { "A", "B", "C", "D", "C" };
+          // Ask the user to select text to search for in the list.
+          Console.WriteLine(" Please type one of these letters ; A,B,C,D,");
+          string word = Console.ReadLine();
+
+           //Create a loop that iterates through the list.
+           
+         for (int i = 0; i < Letters.Count; i++)
 
 
 
-//            for (int i = 0; i < Letters.Count; i++)
+            {
+                if (Letters[i] == word.ToUpper())
+                 {
+                   Console.WriteLine("The Letter you choose is at index " + i);
+                }
 
 
+                // Add code to the loop to check if the user put in text that isn't on the list, and if they did, tells the user their input is not on the list.
+            }
+            if (Letters.Contains(word.ToUpper())== false)
+            {
+               Console.WriteLine("Your input is not in the list");
+            }
+            Console.ReadLine();
 
-//            {
-//                if (Letters[i] == word.ToUpper())
-//                {
-//                    Console.WriteLine("The Letter you choose is at index " + i);
-//                }
-
-
-
-//            }
-//            if (Letters.Contains(word.ToUpper())== false)
-//            {
-//                Console.WriteLine("Your input is not in the list");
-//            }
-//            Console.ReadLine();
-
-//        }
-//    }
-//}
-
-
-
-
-
-
-
-
+        }
+     }
+}
 
 
 
 
 
 
+                    // Part 6
 
+          //Create a list of string that has atleast two indentical strings in the list.
+      List<string> Letters = new List<string>() { "A", "B", "C", "D", "C" };
+       // Second Table empty  created to do the comparison
+     List<string> Array = new List<string>() { };
+     //The foreach loop displays the message of the items on the screen.
+          foreach (string items in Letters)
+       {
+        if (Array.Contains(items))
+       {
+        Console.WriteLine(items + " is a duplicate");
+       }
+       else if (!Array.Contains(items))  
+       {
+          Array.Add(items);
+          Console.WriteLine(items + " is not a duplicate");
+       }
 
-//// Part 6
-////This console code list all the items that will appear on the screen.
-//List<string> Letters = new List<string>() { "A", "B", "C", "D", "C" };
-//// Second Table created to do the comparison
-//List<string> Array = new List<string>() { };
-////The foreach loop displays the message of the items on the screen.
-//foreach (string items in Letters)
-//{
-//    if (Array.Contains(items))
-//    {
-//        Console.WriteLine(items + " is a duplicate");
-//    }
-//    else if (!Array.Contains(items))  
-//        {
-//          Array.Add(items);
-//        Console.WriteLine(items + " is not a duplicate");
-//        }
+}
 
-//}
+     Console.ReadLine();
 
-//Console.ReadLine();
-
-//}
-//}
-//} 
+      }
+   }
+} 
 
