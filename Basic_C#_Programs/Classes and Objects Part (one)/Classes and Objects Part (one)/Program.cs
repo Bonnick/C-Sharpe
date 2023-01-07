97d1f16a7cf4b7b92944bb3b026c9d1fd6263dac
Bonnick <bonnickromario@gmail.com>
2023-01-06 20:30:00 -0500

    Classes and Object Part one - three

---
+++ Basic_C#_Programs/Classes and Objects Part (one)/Classes and Objects Part (one)/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_and_Objects_Part__one_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck deck = new Deck();
+            //deck.Cards = new List<Card>();
+
+
+
+            //Card cardOne = new Card();
+            //cardOne.Face = "Queen";
+            //cardOne.Suit = "Spades";
+
+            //deck.Cards.Add(cardOne);
+
+            foreach ( Card card in deck.Cards)
+            {
+                Console.WriteLine(card.Face + " of " + card.Suit);
+            }
+
+            Console.WriteLine(deck.Cards.Count);
+
+            //Console.WriteLine(deck.Cards[01].Face  + " of " + deck.Cards[0].Suit);
+            Console.ReadLine();
+        }
+    }
+}
