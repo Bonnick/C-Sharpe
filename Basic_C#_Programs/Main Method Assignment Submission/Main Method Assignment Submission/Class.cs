9f762a10578790e363da5f3d36f1782126d619fc
Bonnick <bonnickromario@gmail.com>
2023-01-10 07:14:29 -0500

    Main Method assignment

---
+++ Basic_C#_Programs/Main Method Assignment Submission/Main Method Assignment Submission/Class.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method_Assignment_Submission
+{
+    public class Play
+    {
+
+       
+            public static int results(int myChild)
+            {
+                return myChild  + 2; // Two is added to myNum
+            }
+            public static decimal results2(decimal myChild)
+            {
+                return myChild * 2.64m; // myNum is being multiplied by two
+            }
+            public static string results3( string myChild)
+            {
+
+            var num = 0;
+            for (var i = 0; i < myChild.Length; i++)
+            {
+              num = num * 10 + (myChild[i] - '0');
+            }
+            return myChild;
+
+            
+
+            
+        }
+
+        
+    }
+ }
+
+
+
---
+++ Basic_C#_Programs/Main Method Assignment Submission/Main Method Assignment Submission/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method_Assignment_Submission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // In the main() method of a console app, instantiate the class and call the one method, passing in an integer. Display the result to the screen.
+            Console.WriteLine(Play.results(10));
+
+            //In the main() method of the console app, instantiate the class and call the second method, passing in a decimal. Display the results to the screen. 
+            Console.WriteLine(Play.results2(20m));
+
+            //In the Main() method of the console app, instantiate the class and call the third method, passing in a string that equates to an integer. Display the result to the screen.
+
+
+                Console.WriteLine(myChild[i] + num);
+            
+
+
+
+
+
+                Console.ReadLine();
+        
+        } 
+    }
+}
