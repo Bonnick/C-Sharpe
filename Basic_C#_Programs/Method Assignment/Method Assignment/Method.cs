39c82c1348230f5f6fd07df506ebc70b5565cb9d
Bonnick <bonnickromario@gmail.com>
2023-01-11 07:56:14 -0500

    Method Assignment

---
+++ Basic_C#_Programs/Method Assignment/Method Assignment/Method.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Assignment
+{
+    class Method
+    {
+
+        //Create a class. In that class, create a method that takes two integers as parameters. Make one of them optional. Have the method do a math operation and return an integer result.
+        public static int results(int myNum, int myNum2 = 2)
+        {
+            int sum = 0;
+            sum = myNum + myNum2 + 2;
+            return sum; 
+        }
+     
+
+    }
+}
---
+++ Basic_C#_Programs/Method Assignment/Method Assignment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //In the Main() method of the console app, instantiate the class.
+            Method result = new Method();
+
+            //Ask the user to input two numbers, one at a time. Let them know they need not enter anything for the second number.
+            Console.WriteLine("Please input two number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            
+            //Call the method in the class, passing in the one or two numbers entered.
+
+            Console.WriteLine("Your number is: " + result +
+                result);
+            Console.WriteLine(Method.results(number));
+           
+            
+            
+            Console.ReadLine();
+
+
+            
+
+        }
+    }
+}
