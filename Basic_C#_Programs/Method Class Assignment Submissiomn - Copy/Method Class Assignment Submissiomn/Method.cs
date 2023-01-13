d9c5a233b5471ad515050f2ab878beec667b2d49
Bonnick <bonnickromario@gmail.com>
2023-01-13 07:27:16 -0500

    Method Assignment finale

---
+++ Basic_C#_Programs/Method Class Assignment Submissiomn - Copy/Method Class Assignment Submissiomn/Method.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_Assignment_Submissiomn
+{
+    class Method
+    {
+        public int results(int myNum, int myNum2 = 2)
+        {
+            Console.WriteLine("The second integer is myNum2 = 2");
+
+            int sum = 0;
+            sum = myNum + myNum2 + 2;
+
+            return sum;
+
+            // The second integer will be displayed on the screen along with the mathemational operation of the first integer. 
+
+            //Console.WriteLine("mynum2 " + sum);
+           
+
+        }
+
+
+    }
+}
+
---
+++ Basic_C#_Programs/Method Class Assignment Submissiomn - Copy/Method Class Assignment Submissiomn/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_Assignment_Submissiomn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Method play = new Method();
+            // Call a method in the Class passing in two numbers. 
+            Console.WriteLine("Passing in number one");
+            int number = Convert.ToInt32(Console.ReadLine());
+            //Call a method in the class passing in two numbers.
+            Console.WriteLine("Passing in number two");
+            string number2 = (Console.ReadLine());
+
+            //Call the method in the class, specifying the parameters by name. 
+
+            Object one = new Object();
+            //number2 = one.results(number);
+            Console.WriteLine(play.results(number));
+
+            Console.ReadLine();
+
+
+
+
+
+            }
+        }
+    }
+
+
+
