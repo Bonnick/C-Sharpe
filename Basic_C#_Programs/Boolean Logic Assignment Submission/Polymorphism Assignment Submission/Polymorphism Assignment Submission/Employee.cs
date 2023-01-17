427d8e752ff2ee2c5dbce28ea411e3857040477f
Bonnick <bonnickromario@gmail.com>
2023-01-16 22:46:53 -0500

    Polymorphism Assignment Submission

---
+++ Basic_C#_Programs/Boolean Logic Assignment Submission/Polymorphism Assignment Submission/Polymorphism Assignment Submission/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism_Assignment_Submission
+{
+    //Have your Employee class from the previous drill inherit that interface and implement the Quit() method in any way you choose.
+     class Employee : IQuittable
+    {
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public void Quit( Employee employee)
+        {
+
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+
+
+        }
+
+        internal string Quit()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Basic_C#_Programs/Boolean Logic Assignment Submission/Polymorphism Assignment Submission/Polymorphism Assignment Submission/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism_Assignment_Submission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Use polymorphism to create an object of type IQuittable and call the Quit() method on it. Hint: an object can be of an interface type if it implements that specific interface.
+
+
+            Employee employee = new Employee();
+            employee.FirstName = "Romario";
+            employee.LastName = "Bonnick";
+            Console.WriteLine(employee.Quit());
+            Console.ReadLine();
+
+
+            
+        }
+
+        
+    }
+}
