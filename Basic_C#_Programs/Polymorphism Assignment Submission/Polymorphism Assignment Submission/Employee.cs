b35a023626d6c50286cb06a344f7a1dbc6ed4def
Bonnick <bonnickromario@gmail.com>
2023-01-17 22:58:39 -0500

    Polymorphism Assignment Submission

---
+++ Basic_C#_Programs/Polymorphism Assignment Submission/Polymorphism Assignment Submission/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism_Assignment_Submission
+{
+    //Have your Employee class from the previous drill inherit that interface and implement the Quit() method in any way you choose.
+    public class Employee : IQuittable
+    {
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+
+         void IQuittable.Quit()
+            //(Employee employee)
+        {
+
+            Console.WriteLine("Name: Romario Bonnick" );
+
+
+        }
+
+
+    }
+}
