76c16bd3d399919355826b284e6f83cd512ceb08
Bonnick <bonnickromario@gmail.com>
2023-01-19 06:49:37 -0500

    Parameter Assignment

---
+++ Basic_C#_Programs/Parameters Assignment/Parameters Assignment/Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameters_Assignment
+{
+    public class Employee<T>
+    {
+        //Make the Employee class take a generic type parameter.
+        public List<T> Associates { get; set; }
+        //Add a property to the Employee class called “things” and have its data type be a generic list matching the generic type of the class.
+
+        public string Thing { get; set; }
+
+        //Instantiate an Employee object with type “string” as its generic parameter. Assign a list of strings as the property value of Things.
+
+        List<string> employee = new List<string>
+        {
+            "Tom Brown",
+            "Raymon Thomas",
+            "John Tom",
+            "Marlesa Ben"
+        };
+
+        //Instantiate an Employee object with type “int” as its generic parameter. Assign a list of integers as the property value of Things.
+        List<int> income = new List<int>
+        {
+            1000,
+            2000,
+            3000,
+            8000
+
+        };
+
+            
+         
+
+     }
+
+    
+}
---
+++ Basic_C#_Programs/Parameters Assignment/Parameters Assignment/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameters_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a loop that prints all of the Things to the Console.
+
+
+            Console.WriteLine("Names of Employee");
+            List<string> employee = new List<string>
+        {
+            "Tom Brown",
+            "Raymon Thomas",
+            "John Tom",
+            "Marlesa Ben"
+        };
+            foreach (var item in employee)
+            {
+                Console.WriteLine(employee);
+            }
+            Console.WriteLine("Employee income");
+            List<int> income = new List<int>
+        {
+            1000,
+            2000,
+            3000,
+            8000
+
+        };
+
+            Console.ReadLine();
+        }
+
+        
+    }
+}
