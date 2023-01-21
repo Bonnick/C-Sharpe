8c4f4935f02da68afb04b0fc9fc947e1f6ac2b5e
Bonnick <bonnickromario@gmail.com>
2023-01-21 09:43:02 -0500

    Parsing Enums Assignment Submission

---
+++ Basic_C#_Programs/Parsing Enums Assignment Submission/Parsing Enums Assignment Submission/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parsing_Enums_Assignment_Submission
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           try
+            {
+
+
+                //int myNum = (int)DayOfWeek.Monday;
+
+                //Console.WriteLine(myNum);
+
+                Weeks days = new Weeks();
+                Console.WriteLine("Enter the current day of the week");
+                int myNum = (int)DayOfWeek.Monday;
+                int myNum = Convert.ToInt32(Console.ReadLine(DayOfWeek));
+                
+
+            }
+
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please enter an actual day of the week");
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ Basic_C#_Programs/Parsing Enums Assignment Submission/Parsing Enums Assignment Submission/Weeks.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parsing_Enums_Assignment_Submission
+{
+    public class Weeks
+    {
+        public DayOfWeek DayOfWeek { get; set; }
+    }
+
+    public enum DayOfWeek
+    {
+        Monday=1,
+       Tuesday=12,
+       Wednesday=14,
+       Thursday=15, 
+       Friday=10,
+       Saturday=20,
+       Sunday=40
+
+    }
+}
