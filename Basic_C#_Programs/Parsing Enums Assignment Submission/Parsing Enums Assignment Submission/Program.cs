ce97be635655842446ec1548f5ce1dba850ab134
Bonnick <bonnickromario@gmail.com>
2023-01-21 16:47:49 -0500

    Parsing Enums Assignment Finale update

--- Basic_C#_Programs/Parsing Enums Assignment Submission/Parsing Enums Assignment Submission/Program.cs
+++ Basic_C#_Programs/Parsing Enums Assignment Submission/Parsing Enums Assignment Submission/Program.cs
@@ -8,40 +8,56 @@ namespace Parsing_Enums_Assignment_Submission
 {
     public class Program
     {
+        //Create an enum for the days of the week.
+        public enum DayOfWeek
+        {
+            Monday,
+            Tuesday ,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday 
+
+        }
+
         static void Main(string[] args)
         {
+            //Prompt the user to enter the current day of the week.
+            Console.WriteLine("Enter the current Day of the Week");
+            string day = Console.ReadLine();
 
-            string[] DayOfWeek = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
-            foreach (string Weeks in DayOfWeek)
+            //Wrap the above statement in a try/catch block and have it print "Please enter an actual day of the week.” to the console if an error occurs.
+
+            try
             {
 
+           //Assign the value to a variable of that enum data type you just created.
 
-
-                try
-                {
-
-                    Console.WriteLine("Enter the Days of the Week");
-                    int Days = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("The current Day of the week" + Days);
-                }
-
-
-                catch (FormatException ex)
-                {
-                    Console.WriteLine("Please enter an actual day of the week");
-                }
-
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-                finally
-                {
-                    Console.ReadLine();
-                }
-
+                DayOfWeek week = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), day);
+                 
+                Console.WriteLine("Oh yes the day of the week is " + week);
+            
             }
+
+               
+                 catch (FormatException ex)
+                   {
+                       Console.WriteLine("Please enter an actual day of the week" + day);
+                  }
+
+                   catch (Exception ex)
+                   {
+                       Console.WriteLine(ex.Message);
+                   }
+                   finally
+                   {
+                       Console.ReadLine();
+                   }
+
+             
         }
     }
 }
+
 
