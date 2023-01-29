9a0f3332520f94f63e9e258032b19e95f988a3d4
Bonnick <bonnickromario@gmail.com>
2023-01-29 10:48:24 -0500

    DateTime Assignment

---
+++ Basic_C#_Programs/Datetime Assignment/Datetime Assignment/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datetime_Assignment
+{
+
+
+    }
+
+
+
+    class Program
+    {
+
+   
+        
+
+
+        
+        
+        public static void Main(string[] args)
+        {
+       
+        
+        // Print the current date and time to the console.
+        
+        DateTime now = DateTime.Now;
+        Console.WriteLine(now);
+        Console.ReadLine();
+
+       // Ask the user for a number
+
+        Console.WriteLine("Year: ");
+        var year = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Month: ");
+        var month = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Day: ");
+        var day = Convert.ToInt32(Console.ReadLine());
+
+        // Print to the console the exact time it will be in X hours, X being the number the user entered in step 2.
+
+        Console.WriteLine("The current date and time is " +   year +  month +  day  +  0,  0,  0,  0 );
+        Console.ReadLine();
+        
+
+
+
+
+
+
+    }
+
+
+    }
+
