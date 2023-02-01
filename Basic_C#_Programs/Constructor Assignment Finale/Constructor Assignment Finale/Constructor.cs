a55062e55cb79fabee26b127ac2e317798bc59fe
Bonnick <bonnickromario@gmail.com>
2023-01-31 19:11:55 -0500

    Constructor Finale assignment

---
+++ Basic_C#_Programs/Constructor Assignment Finale/Constructor Assignment Finale/Constructor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Class1
+{
+
+	// Chain two constructors together
+
+	public Class1(string myNum2): this(15)
+	{
+		myNum2 = "Jack";
+
+		return myNum2;
+
+
+	
+	}
+
+
+	public Class1( int myNum)
+    {
+		 myNum = 15;
+
+		return myNum;
+
+
+    }
+}
---
+++ Basic_C#_Programs/Constructor Assignment Finale/Constructor Assignment Finale/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Assignment_Finale
+{
+
+    
+    class Program
+
+        
+    {
+        static void Main(string[] args)
+        { 
+            
+            
+            
+            // Create a const Variable
+            const int myNum = 15;
+
+            Console.WriteLine(myNum);
+
+            // Create a variable using the keyword var
+            var place = "Geeks";
+            Console.WriteLine(place);
+
+          
+
+
+
+
+        }
+    }
+}
