821c2cecd2e1149593575b3780d721026447c092
Bonnick <bonnickromario@gmail.com>
2023-02-14 23:54:41 -0500

    Challenge  Assignment Module 12

---
+++ Basic_C#_Programs/CAS Module 12/Challenge Assignment Submission Module 12/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Challenge_Assignment_Submission_Module_12.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
